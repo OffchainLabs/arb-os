965e9dc746829fae464f4015ee4ab502fabd5801
Ed Felten <ed@felten.com>
2020-05-23 08:33:39 -0400

    Add copyright notices

--- src/evm.rs
+++ src/evm.rs
@@ -1,4 +1,20 @@
-use std::usize;
+/*
+ * Copyright 2020, Offchain Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ use std::usize;
 use std::fs::File;
 use std::path::Path;
 use std::io::Read;
@@ -146,7 +162,6 @@ pub fn compile_evm_insn(
     label_gen: LabelGenerator,
     evm_func_map: &HashMap<&str, Label>,
 ) -> Option<(Vec<Instruction>, LabelGenerator)> {
-    println!("insn {:2x}", evm_insn);
     match evm_insn {
         0x00 => evm_emulate(code, label_gen, evm_func_map, "evmOp_stop"), // STOP
         0x01 => { // ADD
