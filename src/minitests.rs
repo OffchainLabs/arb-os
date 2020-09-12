69495833765e8a4b32e8ee5078d7d0e9cb6e5864
Ed Felten <ed@felten.com>
2020-09-12 09:18:02 -0400

    Add SHA256 precompile test

--- src/minitests.rs
+++ src/minitests.rs
@@ -351,6 +351,9 @@ fn test_direct_deploy_add() {
 }
 
 #[test]
+fn test_sha256_precompile() { crate::evm::evm_eval_sha256(None, false); }
+
+#[test]
 fn test_deploy_buddy_contract() {
     crate::evm::evm_deploy_buddy_contract(None, false);
 }
