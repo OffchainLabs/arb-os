f3c7af9bf1d271cb1505c3769f586653e9e5e5fc
Ed Felten <ed@felten.com>
2020-10-24 08:13:44 -0400

    Suppress compiler warning

--- src/evm/mod.rs
+++ src/evm/mod.rs
@@ -471,6 +471,7 @@ pub fn evm_deploy_buddy_contract(log_to: Option<&Path>, debug: bool) {
     }
 }
 
+#[cfg(test)]
 pub fn evm_test_payment_in_constructor(log_to: Option<&Path>, debug: bool) {
     let rt_env = RuntimeEnvironment::new(Uint256::from_usize(1111));
     let mut machine = load_from_file(Path::new("arb_os/arbos.mexe"), rt_env);
