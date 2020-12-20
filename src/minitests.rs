da62d768ed1b145108f55441803180e8c2068bcd
Ed Felten <ed@felten.com>
2020-12-20 13:31:52 -0500

    Fix merge integration issue

--- src/minitests.rs
+++ src/minitests.rs
@@ -205,7 +205,7 @@ fn test_fixedpoint() {
     let res = run_from_file(
         path,
         vec![],
-        RuntimeEnvironment::new(Uint256::from_usize(1111)),
+        RuntimeEnvironment::new(Uint256::from_usize(1111), None),
         false,
     );
     match res {
