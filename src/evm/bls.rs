1ab24619d47b8488bc9da762d1ceb091179c8965
Steven Goldfeder <hkalodner@MBP18.home>
2020-12-27 19:38:28 -0500

    bls in progress

--- src/evm/bls.rs
+++ src/evm/bls.rs
@@ -7,6 +7,12 @@ use crate::run::{load_from_file, Machine, RuntimeEnvironment};
 use crate::uint256::Uint256;
 use ethers_signers::{Signer, Wallet};
 use std::path::Path;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use num_bigint::BigUint;
+use num_bigint::ToBigUint;
+use num_integer::Integer;
+use std::cmp::Ordering;
 
 pub struct _ArbBLS<'a> {
     pub contract_abi: AbiForContract,
@@ -152,6 +158,247 @@ pub fn _evm_test_bls_registry(log_to: Option<&Path>, debug: bool) {
     }
 }
 
+
 // use bn::{pairing, AffineG1, AffineG2, Fq, Fq2, Group, Gt, G1, G2};
 // ADD HERE: BLS key generation, BLS signature on a &[u8] with a key, aggregation of signatures
+
+
+
+fn hash_to_point(
+    domain: &[u8],
+    msg: &[u8]
+    ) -> () {
+
+    let (u0, u1) = hashToField(domain, msg);
+    let p0 = map_to_g1(&u0);
+    let p1 = map_to_g1(&u1);
+
+//     let x = asm(p0.0, p0.1, p1.0, p1.1) uint { ecadd }; // check order
+//     let y = asm() uint { };
+
+
+
+//     return Some(struct { x: x, y: y });
+}
+
+
+
+fn map_to_g1(_x: &BigUint) -> (Option<(BigUint)>, Option<(BigUint)>) {
+        let field_order = BigUint::parse_bytes(b"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", 16).unwrap();
+        // sqrt(-3)
+    let z0 = BigUint::parse_bytes(b"0000000000000000b3c4d79d41a91759a9e4c7e359b6b89eaec68e62effffffd", 16).unwrap();
+
+// (sqrt(-3) - 1)  / 2
+    let z1 = BigUint::parse_bytes(b"000000000000000059e26bcea0d48bacd4f263f1acdb5c4f5763473177fffffe", 16).unwrap();
+
+        if(_x.cmp(&field_order) != Ordering::Less) {
+            return (None,None)
+        } 
+
+        let sqrtX = sqrt(_x);
+
+        let mut found = false;
+
+        if sqrtX.is_some() {
+        	found = true;
+        }
+
+        let mut a0 = (_x * _x).mod_floor(&field_order);
+        a0 = (a0 + &ToBigUint::to_biguint(&4).unwrap()).mod_floor(&field_order);
+        let mut a1 = (_x * z0).mod_floor(&field_order);
+        let mut a2 = (&a0 * &a1).mod_floor(&field_order);
+        a2 = inverse(&a2);
+        a1 = (&a1 * &a1).mod_floor(&field_order);
+        a1 = (&a1 * &a2).mod_floor(&field_order);
+        
+        a1 = (_x * &a1).mod_floor(&field_order);
+        
+        let mut x = (z1 + (&field_order-&a1)).mod_floor(&field_order);
+
+        a1 = (&x * &x).mod_floor(&field_order);
+        a1 = (&a1 * &x).mod_floor(&field_order);
+        a1 = (&a1 + &ToBigUint::to_biguint(&3).unwrap()).mod_floor(&field_order);
+
+        let mut sqrt_a1 = sqrt(&a1);
+
+         if let Some(sqa1) = sqrt_a1 {
+        	if (found) {
+                a1 = sqa1;
+    	    } else {
+     	    	a1 = &field_order - sqa1;
+     	    }
+            return (Some(x),Some(a1));
+        }
+
+        x = (x + &ToBigUint::to_biguint(&1).unwrap()).mod_floor(&field_order);  
+        x = &field_order - &x;
+
+        a1 = (&x * &x).mod_floor(&field_order);
+        a1 = (&a1 * &x).mod_floor(&field_order);
+        a1 = (&a1 + &ToBigUint::to_biguint(&3).unwrap()).mod_floor(&field_order);
+
+
+        sqrt_a1 = sqrt(&a1);
+
+         if let Some(sqa1) = sqrt_a1 {
+        	if (found) {
+                a1 = sqa1;
+    	    } else {
+     	    	a1 = &field_order - sqa1;
+     	    }
+            return (Some(x),Some(a1));
+        }
+
+
+        x = (&a0 * &a0).mod_floor(&field_order);
+        x = (&x * &x).mod_floor(&field_order);
+        x = (&x * &a2).mod_floor(&field_order);
+        x = (&x * &a2).mod_floor(&field_order);
+        x = (&x + &ToBigUint::to_biguint(&1).unwrap()).mod_floor(&field_order);
+
+        a1 = (&x * &x).mod_floor(&field_order);
+        a1 = (&a1 * &x).mod_floor(&field_order);
+        a1 = (&a1 + &ToBigUint::to_biguint(&3).unwrap()).mod_floor(&field_order);
+
+        sqrt_a1 = sqrt(&a1);
+
+         if let Some(sqa1) = sqrt_a1 {
+        	if (found) {
+                a1 = sqa1;
+    	    } else {
+     	    	a1 = &field_order - sqa1;
+     	    }
+            return (Some(x),Some(a1));
+        }
+
+        (None, None)
+
+
+}
+
+
+fn sqrt(xx: &BigUint) -> Option<BigUint> {
+        let field_order = BigUint::parse_bytes(b"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", 16).unwrap();
+        let field_orderplus1_div4 = BigUint::parse_bytes(b"0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52", 16).unwrap();
+        let x = xx.modpow(&field_orderplus1_div4,&field_order);
+
+        let square = x.modpow(&ToBigUint::to_biguint(&2).unwrap(),&field_order);
+    if(&square == xx) {
+        Some(x)
+    } else {
+        None
+    }
+}
+
+fn inverse(xx: &BigUint) -> BigUint {
+        let field_order = BigUint::parse_bytes(b"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", 16).unwrap();
+        let field_ordermin2 = BigUint::parse_bytes(b"0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd45", 16).unwrap();
+        xx.modpow(&field_ordermin2,&field_order)
+}
+
+fn hashToField(
+    domain: &[u8],
+    msg: &[u8]
+    ) ->(BigUint,BigUint){
+        let _msg = expandMsgTo96(domain, msg);
+    
+    let field_order = BigUint::parse_bytes(b"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", 16).unwrap();
+    let a = BigUint::from_bytes_be(&_msg[0..48]).mod_floor(&field_order);
+    let b = BigUint::from_bytes_be(&_msg[48..96]).mod_floor(&field_order);
+    (a,b)
+
+
+    }
+
+pub fn expandMsgTo96(
+    domain: &[u8],
+    msg: &[u8],
+)->Vec<u8>{
+        if (domain.len() > 32) {
+        panic!(); //todo fix error handling
+        }
+
+    let t0 = msg.len();
+    let t1 = domain.len();
+    let mut out = vec![0; 96];
+    let len0 = 64 + t0 + t1 + 4;
+    let mut in0 = vec![0; len0];
+    
+    // zero pad
+    let mut off = 64;
+    // msg
+    in0[off..off+t0].clone_from_slice(msg);
+    off += t0;
+
+    in0[off] = 0;
+    off +=1;
+    in0[off] = 96;
+    off +=1;
+    in0[off] = 0;
+    off +=1;
+
+    in0[off..off+t1].clone_from_slice(domain);
+    off += t1;
+    in0[off] = t1 as u8;
+
+    let mut hasher = Sha256::new();
+    hasher.input(&in0);
+    let mut b0 = vec![0; 32];
+     hasher.result(&mut b0);
+
+    let len1 = 32 + 1 + domain.len() + 1;
+
+    let mut in1 = vec![0; len1];
+    in1[0..32].clone_from_slice(&b0);
+    off = 32;
+
+    in1[off] = 1;
+    off+=1;
+    in1[off..off+t1].clone_from_slice(domain);
+    off += t1;
+    in1[off] = t1 as u8;
+
+    hasher.reset();
+    hasher.input(&in1);
+    let mut bi = vec![0; 32];
+    hasher.result(&mut bi);
+
+    out[0..32].clone_from_slice(&bi);
+
+    let mut t: Vec<u8> = b0.iter().zip(bi.iter()).map(|(&l, &r)| l ^ r).collect(); 
+
+    in1[0..32].clone_from_slice(&t);
+
+    off = 32;
+    in1[off] = 2;
+    off += 1;
+
+    in1[off..off+t1].clone_from_slice(domain);
+    off = off + t1;
+    in1[off] = t1 as u8;
+
+    hasher.reset();
+    hasher.input(&in1);
+    hasher.result(&mut bi);
+
+    out[32..64].clone_from_slice(&bi);
+
+    t = b0.iter().zip(bi.iter()).map(|(&l, &r)| l ^ r).collect(); 
+
+    in1[0..32].clone_from_slice(&t);
+
+    off = 32;
+    in1[off] = 3;
+    off = off + 1;
+    
+    in1[off..off+t1].clone_from_slice(domain);
+    off = off + t1;
+    in1[off] = t1 as u8;
+
+    hasher.reset();
+    hasher.input(&in1);
+    hasher.result(&mut bi);
+    out[64..96].clone_from_slice(&bi);
+    out
+}
 
