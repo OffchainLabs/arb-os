de79fca59293512a0541494e3aff166e4a2144ef
Ed Felten <ed@felten.com>
2021-03-02 08:09:29 -0500

    Update retry buffer timeout default

--- src/compile/miniconstants.rs
+++ src/compile/miniconstants.rs
@@ -212,7 +212,7 @@ pub fn init_constant_table() -> HashMap<String, Uint256> {
         ("PluggableModuleID_rollupTracker", 0),
         ("PluggableModuleID_precompile_0x05", 1),
         // retry buffer
-        ("RetryBuffer_DefaultLifetimeSeconds", 600),
+        ("RetryBuffer_DefaultLifetimeSeconds", 60*60*24*7),
         // misc
         ("SecondsPerBlockNumerator", 2),
         ("SecondsPerBlockDenominator", 1),
