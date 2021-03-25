4be7ce8ae97d7cd8ca4ec70a92b95352b2db8897
Ed Felten <ed@felten.com>
2021-03-25 08:12:22 -0400

    Auto-generate function codes for builtin contracts

--- src/run/emulator.rs
+++ src/run/emulator.rs
@@ -1335,8 +1335,9 @@ impl Machine {
                         self.incr_pc();
                         Ok(true)
                     }
-                    AVMOpcode::Zero |
-                    AVMOpcode::Panic => Err(ExecutionError::new("panicked", &self.state, None)),
+                    AVMOpcode::Zero | AVMOpcode::Panic => {
+                        Err(ExecutionError::new("panicked", &self.state, None))
+                    }
                     AVMOpcode::Jump => {
                         self.state = MachineState::Running(self.stack.pop_codepoint(&self.state)?);
                         Ok(true)
