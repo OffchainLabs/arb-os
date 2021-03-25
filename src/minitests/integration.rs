33f0af5193b49cc2c26b67ecf104a779f3992ad0
rgates94 <rgates275@gmail.com>
2021-03-25 12:05:10 -0400

    Add integration test for codeblocks

--- src/minitests/integration.rs
+++ src/minitests/integration.rs
@@ -2,6 +2,7 @@ use crate::compile::CompileStruct;
 use crate::mavm::Value;
 use crate::run::{run, Machine, RuntimeEnvironment};
 use crate::uint256::Uint256;
+use std::rc::Rc;
 
 #[test]
 fn test_basic() {
@@ -28,4 +29,24 @@ fn test_xif_else() {
     );
     let _result = run(&mut machine, vec![], false).unwrap();
     assert_eq!(machine.stack_top(), Some(&Value::Int(Uint256::zero())));
+}
+
+#[test]
+fn test_codeblocks() {
+    let mut compile = CompileStruct::default();
+    compile.input = vec!["test-programs/codeblocks.mini".to_string()];
+    compile.test_mode = true;
+    let mexe = compile.invoke().unwrap();
+    let mut machine = Machine::new(
+        mexe,
+        RuntimeEnvironment::new(Uint256::from_usize(1111), None),
+    );
+    let _result = run(&mut machine, vec![], false).unwrap();
+    assert_eq!(
+        machine.stack_top(),
+        Some(&Value::Tuple(Rc::new(vec![
+            Value::Int(Uint256::zero()),
+            Value::Int(Uint256::from_u64(25))
+        ])))
+    );
 }
