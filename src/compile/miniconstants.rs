fdb672268587e2dd15536ea96437dba7785b21e2
Ed Felten <ed@felten.com>
2021-04-11 19:05:04 -0400

    Bump ArbOS version number to 4

--- src/compile/miniconstants.rs
+++ src/compile/miniconstants.rs
@@ -8,7 +8,7 @@ use crate::evm::abi::{builtin_contract_path, AbiForContract};
 use crate::uint256::Uint256;
 use std::collections::HashMap;
 
-pub static ARBOS_VERSION: u64 = 3;
+pub static ARBOS_VERSION: u64 = 4;
 
 ///Creates a fixed list of globally accessible constants.
 pub fn init_constant_table() -> HashMap<String, Uint256> {
