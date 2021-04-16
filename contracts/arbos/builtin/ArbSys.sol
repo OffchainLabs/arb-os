418ba4ac93a477367da581a52f027e772dda6027
dzgoldman <dzgoldman@wesleyan.edu>
2021-04-16 10:12:17 -0400

    natspec ArbSys; remove deorecated ERC20 and ERC721 events

--- contracts/arbos/builtin/ArbSys.sol
+++ contracts/arbos/builtin/ArbSys.sol
@@ -1,31 +1,61 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+/**
+* @title Precompiled contract that exists in every Arbitrum chain at address(100), 0x0000000000000000000000000000000000000064. Exposes a variety of system-level functionality.
+ */
 interface ArbSys {
-    // Get ArbOS version number
+    /**
+    * @notice  Get ArbOS version number
+    * @return version number int
+     */
     function arbOSVersion() external pure returns (uint);
 
-    // Get Arbitrum block number
+    /**
+    * @notice Get Arbitrum block number (Arbitrum genisis starts at 0, increments with qqq)
+    * @return block number int
+     */ 
     function arbBlockNumber() external view returns (uint);
 
-    // Send given amount of Eth to dest with from sender.
-    // This is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1.
-    // Returns a unique identifier for this L2-to-L1 transaction.
+    /** 
+    * @notice Send given amount of Eth to dest with from sender.
+    * This is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1.
+    * @param destination recipient address on L1
+     * @return unique identifier for this L2-to-L1 transaction.
+    */
     function withdrawEth(address destination) external payable returns(uint);
 
-    // Send a transaction to L1
-    // Returns a unique identifier for this L2-to-L1 transaction.
+    /** 
+    * @notice Send a transaction to L1
+    * @param destination recipient address on L1 
+    * @param calldataForL1 call data for L1 contract call
+    * @return a unique identifier for this L2-to-L1 transaction.
+    */
     function sendTxToL1(address destination, bytes calldata calldataForL1) external payable returns(uint);
 
-    // Return the number of transactions issued by the given external account
-    // or the account sequence number of the given contract
+
+
+    /** 
+    * @notice get the number of transactions issued by the given external account or the account sequence number of the given contract
+    * @param account target  account
+    * @return the number of transactions issued by the given external account or the account sequence number of the given contract
+    */
     function getTransactionCount(address account) external view returns(uint256);
 
-    // Return the value of the storage slot for the given account at the given index
-    // This function is only callable from address 0 to prevent contracts from being
-    // able to call it
+    /**  
+    @notice get the value of the storage slot 
+    * This function is only callable from address 0 to prevent contracts from being
+    * able to call it
+    * @param account target account
+    * @param index target index of storage slot 
+    * @return for the given account at the given index
+
+    */
     function getStorageAt(address account, uint256 index) external view returns (uint256);
 
-    // Return true if the caller of this was called directly from L1
+    /**
+    * @notice check if current call is coming from l1
+    @return returns true if the caller of this was called directly from L1
+    */
     function isTopLevelCall() external view returns (bool);
 
     event EthWithdrawal(address indexed destAddr, uint amount);
