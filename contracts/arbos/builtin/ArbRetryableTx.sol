4371bf7d3640867786736e3230f159eb4f73950e
dzgoldman <dzgoldman@wesleyan.edu>
2021-04-16 10:25:47 -0400

    fix typos; include full definition of retryable txn id calculation in natspec comment

--- contracts/arbos/builtin/ArbRetryableTx.sol
+++ contracts/arbos/builtin/ArbRetryableTx.sol
@@ -11,7 +11,7 @@ interface ArbRetryableTx {
     * Revert if called by an L2 contract, or if txId does not exist, or if txId reverts.
     * If this returns, txId has been completed and is no longer available for redemption.
     * If this reverts, txId is still available for redemption (until it times out or is canceled).
-    @param txId unique identifier of retryabale message: keccak256(requestID, uint(0) )
+    @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
      */
     function redeem(bytes32 txId) external;
 
@@ -24,7 +24,7 @@ interface ArbRetryableTx {
     /**
     * @notice Return the timestamp when txId will age out, or zero if txId does not exist.
     * The timestamp could be in the past, because aged-out txs might not be discarded immediately.
-    * @param txId unique identifier of retryabale message: keccak256(requestID, uint(0) )
+    * @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
     * @return timestamp for txn's deadline  
     */
     function getTimeout(bytes32 txId) external view returns(uint);
@@ -38,7 +38,7 @@ interface ArbRetryableTx {
 
     /** 
      * @notice Return the price, in wei, of extending the lifetime of txId by an additional lifetime period. Revert if txId doesn't exist.
-     * @param txId  unique identifier of retryabale message: keccak256(requestID, uint(0) )
+     * @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
      * @return (price, nextUpdateTimestamp). Price is guaranteed not to change until nextUpdateTimestamp.
     */
     function getKeepalivePrice(bytes32 txId) external view returns(uint, uint);
@@ -47,7 +47,7 @@ interface ArbRetryableTx {
     @notice Deposits callvalue into the sender's L2 account, then adds one lifetime period to the life of txId.
     * If successful, emits LifetimeExtended event.
     * Revert if txId does not exist, or if the timeout of txId is already at least one lifetime in the future, or if the sender has insufficient funds (after the deposit).
-    * @param txId unique identifier of retryabale message: keccak256(requestID, uint(0) )
+    * @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
     * @return New timeout of txId.
     */
     function keepalive(bytes32 txId) external payable returns(uint);
@@ -55,7 +55,7 @@ interface ArbRetryableTx {
     /**
     * @notice Return the beneficiary of txId.
     * Revert if txId doesn't exist.
-    * @param txId unique identifier of retryabale message: keccak256(requestID, uint(0) )
+    * @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
     * @return address of beneficiary for transaction 
     */
     function getBeneficiary(bytes32 txId) external view returns (address);
@@ -63,7 +63,7 @@ interface ArbRetryableTx {
     /** 
     @notice Cancel txId and refund its callvalue to its beneficiary.
     * Revert if txId doesn't exist, or if called by anyone other than txId's beneficiary.
-    @param txId unique identifier of retryabale message: keccak256(requestID, uint(0) )
+    @param txId unique identifier of retryable message: keccak256(keccak256(ArbchainId, inbox-sequence-number), uint(0) )
     */
     function cancel(bytes32 txId) external;
 
