1f3df34e7ccba44e0cc5dbb403b512420046a56a
dzgoldman <dzgoldman@wesleyan.edu>
2021-04-16 10:16:18 -0400

    cleanup, fix typos

--- contracts/arbos/builtin/ArbSys.sol
+++ contracts/arbos/builtin/ArbSys.sol
@@ -17,17 +17,17 @@ interface ArbSys {
     function arbBlockNumber() external view returns (uint);
 
     /** 
-    * @notice Send given amount of Eth to dest with from sender.
+    * @notice Send given amount of Eth to dest from sender.
     * This is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1.
     * @param destination recipient address on L1
-     * @return unique identifier for this L2-to-L1 transaction.
+    * @return unique identifier for this L2-to-L1 transaction.
     */
     function withdrawEth(address destination) external payable returns(uint);
 
     /** 
     * @notice Send a transaction to L1
     * @param destination recipient address on L1 
-    * @param calldataForL1 call data for L1 contract call
+    * @param calldataForL1 (optional) calldata for L1 contract call
     * @return a unique identifier for this L2-to-L1 transaction.
     */
     function sendTxToL1(address destination, bytes calldata calldataForL1) external payable returns(uint);
@@ -36,25 +36,23 @@ interface ArbSys {
 
     /** 
     * @notice get the number of transactions issued by the given external account or the account sequence number of the given contract
-    * @param account target  account
+    * @param account target account
     * @return the number of transactions issued by the given external account or the account sequence number of the given contract
     */
     function getTransactionCount(address account) external view returns(uint256);
 
     /**  
-    @notice get the value of the storage slot 
-    * This function is only callable from address 0 to prevent contracts from being
-    * able to call it
+    * @notice get the value of target L2 storage slot 
+    * This function is only callable from address 0 to prevent contracts from being able to call it
     * @param account target account
     * @param index target index of storage slot 
-    * @return for the given account at the given index
-
+    * @return stotage value for the given account at the given index
     */
     function getStorageAt(address account, uint256 index) external view returns (uint256);
 
     /**
     * @notice check if current call is coming from l1
-    @return returns true if the caller of this was called directly from L1
+    * @return true if the caller of this was called directly from L1
     */
     function isTopLevelCall() external view returns (bool);
 
