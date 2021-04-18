886bbfa729714e4a4469cb5059d4fc678b00d109
Ed Felten <ed@felten.com>
2021-04-18 16:42:37 -0400

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate lalrpop;
 
 fn main() {
-	let mut config = lalrpop::Configuration::new();
-	config.emit_rerun_directives(true);
-	config.process_current_dir().unwrap();
+    let mut config = lalrpop::Configuration::new();
+    config.emit_rerun_directives(true);
+    config.process_current_dir().unwrap();
 }
