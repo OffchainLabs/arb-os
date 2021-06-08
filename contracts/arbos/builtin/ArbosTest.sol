2d5e41e95ce1ecdcd92364f8c584616cb8b6ab34
Ed Felten <ed@felten.com>
2021-06-07 21:34:42 -0400

    Add methods to adjust contract attributes when testing

--- contracts/arbos/builtin/ArbosTest.sol
+++ contracts/arbos/builtin/ArbosTest.sol
@@ -3,6 +3,14 @@ pragma solidity >=0.4.21 <0.7.0;
 interface ArbosTest {
     function installAccount(address addr, bool isEOA, uint balance, uint nonce, bytes calldata code, bytes calldata initStorage) external; 
 
+    function setNonce(address addr, uint nonce) external;
+
+    function setBalance(address addr, uint balance) external;
+
+    function setStorage(address addr, bool isDiff, bytes calldata marshalledStorage) external;
+
+    function setCode(address addr, bytes calldata evmCode) external;
+
     function getMarshalledStorage(address addr) external view;  // returns raw returndata
 
     function getAccountInfo(address addr) external view;  // returns raw returndata
