f3b554c11be3f7e3a61b6ec19ec34e4dff7361cf
Sami Mäkelä <mrsmkl@gmail.com>
2021-07-26 18:23:03 +0300

    tests working

--- src/evm/preinstalled_contracts.rs
+++ src/evm/preinstalled_contracts.rs
@@ -1543,7 +1543,7 @@ fn _test_upgrade_arbos_over_itself_impl() -> Result<(), ethabi::Error> {
     let arbsys_orig_binding = ArbSys::new(&wallet, false);
     assert_eq!(
         arbsys_orig_binding._arbos_version(&mut machine)?,
-        Uint256::from_u64(35),
+        Uint256::from_u64(36),
     );
 
     arbowner._give_ownership(&mut machine, my_addr, true)?;
