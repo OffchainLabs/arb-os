8e758292e59f6eec9bc61e8a2013bcc95fda6b0e
Rachel Franks <nfranks@protonmail.com>
2021-08-26 21:10:27 -0500

    Added correct mexe

--- src/evm/preinstalled_contracts.rs
+++ src/evm/preinstalled_contracts.rs
@@ -1602,7 +1602,7 @@ fn _test_upgrade_arbos_over_itself_impl() -> Result<(), ethabi::Error> {
     let arbsys_orig_binding = ArbSys::new(&wallet, false);
     assert_eq!(
         arbsys_orig_binding._arbos_version(&mut machine)?,
-        Uint256::from_u64(37),
+        Uint256::from_u64(38),
     );
 
     arbowner._add_chain_owner(&mut machine, my_addr.clone(), true, true)?;
