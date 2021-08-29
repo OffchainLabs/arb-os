17bc7a3cb49c9f791a5e5f76a53d3b91dfc276e7
Ed Felten <ed@felten.com>
2021-08-29 14:30:51 -0400

    cargo fmt

--- src/minitests/mod.rs
+++ src/minitests/mod.rs
@@ -940,7 +940,11 @@ fn test_l1_sender_rewrite() {
     let arbsys = ArbSys::new(&wallet, false);
     assert_eq!(
         arbsys
-            .map_l1_sender_contract_address_to_l2_alias(&mut machine, my_addr.clone(), my_addr.clone())
+            .map_l1_sender_contract_address_to_l2_alias(
+                &mut machine,
+                my_addr.clone(),
+                my_addr.clone()
+            )
             .unwrap(),
         remap_l1_sender_address(my_addr.clone())
     );
